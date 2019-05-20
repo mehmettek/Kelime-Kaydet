6fc131731de4e9a46f3a1168f56186b809d3f62c
Mehmet Tek <mehmet_tek@windowslive.com>
2019-05-20 21:37:27 +0300

    Proje dosyası ekle.

---
+++ Kelime Kaydet/Form1.vb
@@ -0,0 +1,36 @@
+﻿Imports System.IO.File
+Imports System.IO
+Public Structure kelimeler
+    Public ing As String
+    Public tur As String
+End Structure
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Focus()
+        veri_oku()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim fs As FileStream = New FileStream("D:\kelimeler.txt", FileMode.Append) 'dosya yolu
+            Dim aa As kelimeler = New kelimeler()
+            aa.ing = TextBox1.Text.ToUpper()
+            aa.tur = TextBox2.Text.ToUpper()
+
+            Dim yaz As StreamWriter = New StreamWriter(fs)
+            yaz.WriteLine("   " + aa.ing + "-----" + aa.tur)
+            yaz.Close()
+            ListBox1.Items.Clear()
+            veri_oku()
+            TextBox1.Focus()
+            TextBox1.Clear()
+            TextBox2.Clear()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Public Function veri_oku()
+        Dim lines() As String = IO.File.ReadAllLines("D:\kelimeler.txt")
+        ListBox1.Items.AddRange(lines)
+    End Function
+End Class
---
+++ Kelime Kaydet/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Kelime Kaydet")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Kelime Kaydet")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("62990cf4-069a-4ff0-81b8-8a2f7e5a6f49")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
